9824651964969ea1d334a43112f23233822d128b
Frank Voorburg <voorburg@feaser.com>
2016-05-25 13:02:32 +0000

    - Removed explicit stackpointer initialization from the STM32-P103/Keil demo user program. It is already done by Keil's C library during system startup.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@143 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Target/Demo/ARMCM3_STM32_Olimex_STM32P103_Keil/Prog/startup_stm32f10x_md.s
+++ Target/Demo/ARMCM3_STM32_Olimex_STM32P103_Keil/Prog/startup_stm32f10x_md.s
@@ -156,10 +156,6 @@ Reset_Handler    PROC
                  EXPORT  Reset_Handler             [WEAK]
      IMPORT  __main
      IMPORT  SystemInit
-		         ; Initialize stackpointer explicitely, because this program is
-				 ; not started by a reset, but by OpenBLT
-		         LDR	 R0, =__initial_sp
-				 MOV	 SP, R0
                  LDR     R0, =SystemInit
                  BLX     R0
                  LDR     R0, =__main
