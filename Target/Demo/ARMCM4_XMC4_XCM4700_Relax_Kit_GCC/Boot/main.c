874b7afe4bc68a7f13090e9172d7519e1e559db0
Frank Voorburg <voorburg@feaser.com>
2016-10-20 15:46:56 +0000

    Refs #123. - Added empty framework and bootloader demo program as a starting point for the Infineon XMC4xxx port on the XMC4700 Relax Kit.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@160 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

---
+++ Target/Demo/ARMCM4_XMC4_XCM4700_Relax_Kit_GCC/Boot/main.c
@@ -0,0 +1,79 @@
+/************************************************************************************//**
+* \file         Demo\ARMCM4_XMC4_XCM4700_Relax_Kit_GCC\Boot\main.c
+* \brief        Bootloader application source file.
+* \ingroup      Boot_ARMCM4_XMC4_XCM4700_Relax_Kit_GCC
+* \internal
+*----------------------------------------------------------------------------------------
+*                          C O P Y R I G H T
+*----------------------------------------------------------------------------------------
+*   Copyright (c) 2016  by Feaser    http://www.feaser.com    All rights reserved
+*
+*----------------------------------------------------------------------------------------
+*                            L I C E N S E
+*----------------------------------------------------------------------------------------
+* This file is part of OpenBLT. OpenBLT is free software: you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as published by the Free
+* Software Foundation, either version 3 of the License, or (at your option) any later
+* version.
+*
+* OpenBLT is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE. See the GNU General Public License for more details.
+*
+* You have received a copy of the GNU General Public License along with OpenBLT. It
+* should be located in ".\Doc\license.html". If not, contact Feaser to obtain a copy.
+*
+* \endinternal
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include "boot.h"                                /* bootloader generic header          */
+
+
+/****************************************************************************************
+* Function prototypes
+****************************************************************************************/
+static void Init(void);
+
+
+/************************************************************************************//**
+** \brief     This is the entry point for the bootloader application and is called
+**            by the reset interrupt vector after the C-startup routines executed.
+** \return    Program return code.
+**
+****************************************************************************************/
+int main(void)
+{
+  /* initialize the microcontroller */
+  Init();
+  /* initialize the bootloader */
+  BootInit();
+
+  /* start the infinite program loop */
+  while (1)
+  {
+    /* run the bootloader task */
+    BootTask();
+  }
+
+  /* program should never get here */
+  return 0;
+} /*** end of main ***/
+
+
+/************************************************************************************//**
+** \brief     Initializes the microcontroller.
+** \return    none.
+**
+****************************************************************************************/
+static void Init(void)
+{
+  /* ##Vg TODO initialize UART Tx/Rx pins. */
+  /* ##Vg TODO initialize pushbutton digital input. */
+  /* ##Vg TODO initialize LED digital output. */
+} /*** end of Init ***/
+
+
+/*********************************** end of main.c *************************************/
