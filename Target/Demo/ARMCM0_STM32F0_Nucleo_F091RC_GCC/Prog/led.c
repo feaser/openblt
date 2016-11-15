5aa8a44d745ac8709061be6ddc82345bfa5ea4c9
Frank Voorburg <voorburg@feaser.com>
2016-11-15 16:08:42 +0000

    Refs #125 - Added ST Nucleo-F091RC demo bootloader and user program.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@183 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

---
+++ Target/Demo/ARMCM0_STM32F0_Nucleo_F091RC_GCC/Prog/led.c
@@ -0,0 +1,103 @@
+/************************************************************************************//**
+* \file         Demo\ARMCM0_STM32F0_Nucleo_F091RC_GCC\Prog\led.c
+* \brief        LED driver source file.
+* \ingroup      Prog_ARMCM0_STM32F0_Nucleo_F091RC_GCC
+* \internal
+*----------------------------------------------------------------------------------------
+*                          C O P Y R I G H T
+*----------------------------------------------------------------------------------------
+*   Copyright (c) 2016  by Feaser    http://www.feaser.com    All rights reserved
+*
+*----------------------------------------------------------------------------------------
+*                            L I C E N S E
+*----------------------------------------------------------------------------------------
+* This file is part of OpenBLT. OpenBLT is free software: you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as published by the Free
+* Software Foundation, either version 3 of the License, or (at your option) any later
+* version.
+*
+* OpenBLT is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE. See the GNU General Public License for more details.
+*
+* You have received a copy of the GNU General Public License along with OpenBLT. It
+* should be located in ".\Doc\license.html". If not, contact Feaser to obtain a copy.
+*
+* \endinternal
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include "header.h"                                    /* generic header               */
+
+
+/****************************************************************************************
+* Macro definitions
+****************************************************************************************/
+/** \brief Toggle interval time in milliseconds. */
+#define LED_TOGGLE_MS  (500)
+
+
+/************************************************************************************//**
+** \brief     Initializes the LED. The board doesn't have a dedicted LED so an
+**            indicator on the LCD is used instead.
+** \return    none.
+**
+****************************************************************************************/
+void LedInit(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* enable the GPIO_LED Clock */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  /* configure the GPIO_LED pin */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+} /*** end of LedInit ***/
+
+
+/************************************************************************************//**
+** \brief     Toggles the LED at a fixed time interval.
+** \return    none.
+**
+****************************************************************************************/
+void LedToggle(void)
+{
+  static unsigned char led_toggle_state = 0;
+  static unsigned long timer_counter_last = 0;
+  unsigned long timer_counter_now;
+
+  /* check if toggle interval time passed */
+  timer_counter_now = TimerGet();
+  if ( (timer_counter_now - timer_counter_last) < LED_TOGGLE_MS)
+  {
+    /* not yet time to toggle */
+    return;
+  }
+
+  /* determine toggle action */
+  if (led_toggle_state == 0)
+  {
+    led_toggle_state = 1;
+    /* turn the LED on */
+    GPIO_SetBits(GPIOA, GPIO_Pin_5);
+  }
+  else
+  {
+    led_toggle_state = 0;
+    /* turn the LED off */
+    GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+  }
+
+  /* store toggle time to determine next toggle interval */
+  timer_counter_last = timer_counter_now;
+} /*** end of LedToggle ***/
+
+
+/*********************************** end of led.c **************************************/
