8a3ea513831d5c649e8042c1f543d900af719171
Frank Voorburg <voorburg@feaser.com>
2018-01-02 20:05:56 +0000

    Refs #310. The STM32 Nucleo-F103RB demo programs now support the IAR Embedded Workbench as well.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@391 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Target/Demo/ARMCM3_STM32F1_Nucleo_F103RB_GCC/Boot/hooks.c
+++ Target/Demo/ARMCM3_STM32F1_Nucleo_F103RB_GCC/Boot/hooks.c
@@ -30,10 +30,8 @@
 * Include files
 ****************************************************************************************/
 #include "boot.h"                                /* bootloader generic header          */
-#if (BOOT_FILE_LOGGING_ENABLE > 0)
-#include "stm32f10x.h"                           /* STM32 registers                    */
-#include "stm32f10x_conf.h"                      /* STM32 peripheral drivers           */
-#endif
+#include "led.h"                                 /* LED driver header                  */
+#include "stm32f10x.h"                           /* microcontroller registers          */
 
 
 /****************************************************************************************
@@ -80,10 +78,64 @@ blt_bool BackDoorEntryHook(void)
 ****************************************************************************************/
 blt_bool CpuUserProgramStartHook(void)
 {
-  /* okay to start the user program */
+  /* clean up the LED driver */
+  LedBlinkExit();
+
+  /* additional and optional backdoor entry through the pushbutton on the board. to
+   * force the bootloader to stay active after reset, keep it pressed during reset.
+   */
+  if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_13) == Bit_RESET)
+  {
+    /* pushbutton pressed, so do not start the user program and keep the
+     * bootloader active instead.
+     */
+    return BLT_FALSE;
+  }
+  /*  okay to start the user program.*/
   return BLT_TRUE;
 } /*** end of CpuUserProgramStartHook ***/
 #endif /* BOOT_CPU_USER_PROGRAM_START_HOOK > 0 */
+
+
+/****************************************************************************************
+*   W A T C H D O G   D R I V E R   H O O K   F U N C T I O N S
+****************************************************************************************/
+
+#if (BOOT_COP_HOOKS_ENABLE > 0)
+/************************************************************************************//**
+** \brief     Callback that gets called at the end of the internal COP driver
+**            initialization routine. It can be used to configure and enable the
+**            watchdog.
+** \return    none.
+**
+****************************************************************************************/
+void CopInitHook(void)
+{
+  /* this function is called upon initialization. might as well use it to initialize
+   * the LED driver. It is kind of a visual watchdog anyways.
+   */
+  LedBlinkInit(100);
+} /*** end of CopInitHook ***/
+
+
+/************************************************************************************//**
+** \brief     Callback that gets called at the end of the internal COP driver
+**            service routine. This gets called upon initialization and during
+**            potential long lasting loops and routine. It can be used to service
+**            the watchdog to prevent a watchdog reset.
+** \return    none.
+**
+****************************************************************************************/
+void CopServiceHook(void)
+{
+  /* run the LED blink task. this is a better place to do it than in the main() program
+   * loop. certain operations such as flash erase can take a long time, which would cause
+   * a blink interval to be skipped. this function is also called during such operations,
+   * so no blink intervals will be skipped when calling the LED blink task here.
+   */
+  LedBlinkTask();
+} /*** end of CopServiceHook ***/
+#endif /* BOOT_COP_HOOKS_ENABLE > 0 */
 
 
 /****************************************************************************************
@@ -190,211 +242,6 @@ blt_bool NvmWriteChecksumHook(void)
   return BLT_TRUE;
 }
 #endif /* BOOT_NVM_CHECKSUM_HOOKS_ENABLE > 0 */
-
-
-/****************************************************************************************
-*   W A T C H D O G   D R I V E R   H O O K   F U N C T I O N S
-****************************************************************************************/
-
-#if (BOOT_COP_HOOKS_ENABLE > 0)
-/************************************************************************************//**
-** \brief     Callback that gets called at the end of the internal COP driver
-**            initialization routine. It can be used to configure and enable the
-**            watchdog.
-** \return    none.
-**
-****************************************************************************************/
-void CopInitHook(void)
-{
-} /*** end of CopInitHook ***/
-
-
-/************************************************************************************//**
-** \brief     Callback that gets called at the end of the internal COP driver
-**            service routine. This gets called upon initialization and during
-**            potential long lasting loops and routine. It can be used to service
-**            the watchdog to prevent a watchdog reset.
-** \return    none.
-**
-****************************************************************************************/
-void CopServiceHook(void)
-{
-} /*** end of CopServiceHook ***/
-#endif /* BOOT_COP_HOOKS_ENABLE > 0 */
-
-
-/****************************************************************************************
-*   F I L E   S Y S T E M   I N T E R F A C E   H O O K   F U N C T I O N S
-****************************************************************************************/
-
-#if (BOOT_FILE_SYS_ENABLE > 0)
-
-/****************************************************************************************
-* Constant data declarations
-****************************************************************************************/
-/** \brief Firmware filename. */
-static const blt_char firmwareFilename[] = "/demoprog_olimex_stm32p103.srec";
-
-
-/****************************************************************************************
-* Local data declarations
-****************************************************************************************/
-#if (BOOT_FILE_LOGGING_ENABLE > 0)
-/** \brief Data structure for grouping log-file related information. */
-static struct 
-{
-  FIL      handle;                  /**< FatFS handle to the log-file.                 */
-  blt_bool canUse;                  /**< Flag to indicate if the log-file can be used. */
-} logfile;
-#endif
-
-
-/************************************************************************************//**
-** \brief     Callback that gets called to check whether a firmware update from 
-**            local file storage should be started. This could for example be when
-**            a switch is pressed, when a certain file is found on the local file 
-**            storage, etc.
-** \return    BLT_TRUE if a firmware update is requested, BLT_FALSE otherwise.
-**
-****************************************************************************************/
-blt_bool FileIsFirmwareUpdateRequestedHook(void)
-{
-  FILINFO fileInfoObject = { 0 }; /* needs to be zeroed according to f_stat docs */;
-
-  /* Current example implementation looks for a predetermined firmware file on the 
-   * SD-card. If the SD-card is accessible and the firmware file was found the firmware
-   * update is started. When successfully completed, the firmware file is deleted.
-   * During the firmware update, progress information is written to a file called
-   * bootlog.txt and additionally outputted on UART @57600 bps for debugging purposes.
-   */
-  /* check if firmware file is present and SD-card is accessible */
-  if (f_stat(firmwareFilename, &fileInfoObject) == FR_OK) 
-  {
-    /* check if the filesize is valid and that it is not a directory */
-    if ( (fileInfoObject.fsize > 0) && (!(fileInfoObject.fattrib & AM_DIR)) )
-    {
-      /* all conditions are met to start a firmware update from local file storage */
-      return BLT_TRUE;
-    }
-  }
-  /* still here so no firmware update request is pending */  
-  return BLT_FALSE;
-} /*** end of FileIsFirmwareUpdateRequestedHook ***/
-
-
-/************************************************************************************//**
-** \brief     Callback to obtain the filename of the firmware file that should be
-**            used during the firmware update from the local file storage. This 
-**            hook function is called at the beginning of the firmware update from
-**            local storage sequence. 
-** \return    valid firmware filename with full path or BLT_NULL.
-**
-****************************************************************************************/
-const blt_char *FileGetFirmwareFilenameHook(void)
-{
-  return firmwareFilename;
-} /*** end of FileGetFirmwareFilenameHook ***/
-
-
-#if (BOOT_FILE_STARTED_HOOK_ENABLE > 0)
-/************************************************************************************//**
-** \brief     Callback that gets called to inform the application that a firmware
-**            update from local storage just started. 
-** \return    none.
-**
-****************************************************************************************/
-void FileFirmwareUpdateStartedHook(void)
-{
-  #if (BOOT_FILE_LOGGING_ENABLE > 0)
-  /* create/overwrite the logfile */
-  logfile.canUse = BLT_FALSE;
-  if (f_open(&logfile.handle, "/bootlog.txt", FA_CREATE_ALWAYS | FA_WRITE) == FR_OK)
-  {
-    logfile.canUse = BLT_TRUE;
-  }
-  #endif
-} /*** end of FileFirmwareUpdateStartedHook ***/
-#endif /* BOOT_FILE_STARTED_HOOK_ENABLE > 0 */
-
-
-#if (BOOT_FILE_COMPLETED_HOOK_ENABLE > 0)
-/************************************************************************************//**
-** \brief     Callback that gets called to inform the application that a firmware
-**            update was successfully completed.
-** \return    none.
-**
-****************************************************************************************/
-void FileFirmwareUpdateCompletedHook(void)
-{
-  #if (BOOT_FILE_LOGGING_ENABLE > 0)
-  /* close the log file */
-  if (logfile.canUse == BLT_TRUE)
-  {
-    f_close(&logfile.handle);
-  }
-  /* wait for all logging related transmission to complete */
-  while(USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET);
-  #endif
-  /* now delete the firmware file from the disk since the update was successful */
-  f_unlink(firmwareFilename);
-} /*** end of FileFirmwareUpdateCompletedHook ***/
-#endif /* BOOT_FILE_COMPLETED_HOOK_ENABLE > 0 */
-
-
-#if (BOOT_FILE_ERROR_HOOK_ENABLE > 0)
-/************************************************************************************//**
-** \brief     Callback that gets called in case an error occurred during a firmware
-**            update. Refer to <file.h> for a list of available error codes.
-** \return    none.
-**
-****************************************************************************************/
-void FileFirmwareUpdateErrorHook(blt_int8u error_code)
-{
-  #if (BOOT_FILE_LOGGING_ENABLE > 0)
-  /* error detected which stops the firmware update, so close the log file */
-  if (logfile.canUse == BLT_TRUE)
-  {
-    f_close(&logfile.handle);
-  }
-  #endif
-} /*** end of FileFirmwareUpdateErrorHook ***/
-#endif /* BOOT_FILE_ERROR_HOOK_ENABLE > 0 */
-
-
-#if (BOOT_FILE_LOGGING_ENABLE > 0)
-/************************************************************************************//**
-** \brief     Callback that gets called each time new log information becomes 
-**            available during a firmware update.
-** \param     info_string Pointer to a character array with the log entry info.
-** \return    none.
-**
-****************************************************************************************/
-void FileFirmwareUpdateLogHook(blt_char *info_string)
-{
-  /* write the string to the log file */
-  if (logfile.canUse == BLT_TRUE)
-  {
-    if (f_puts(info_string, &logfile.handle) < 0)
-    {
-      logfile.canUse = BLT_FALSE;
-      f_close(&logfile.handle);
-    }
-  }
-  /* echo all characters in the string on UART */
-  while(*info_string != '\0')
-  {
-    /* write character to transmit holding register */
-    USART_SendData(USART2, *info_string);
-    /* wait for tx holding register to be empty */
-    while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
-    /* point to the next character in the string */
-    info_string++;
-  }
-} /*** end of FileFirmwareUpdateLogHook ***/
-#endif /* BOOT_FILE_LOGGING_ENABLE > 0 */
-
-
-#endif /* BOOT_FILE_SYS_ENABLE > 0 */
 
 
 /****************************************************************************************
