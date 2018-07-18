bcc4f44a0c744e69f803aac098b15e61de6543e5
Frank Voorburg <voorburg@feaser.com>
2018-07-18 09:19:20 +0000

    Refs #564. Resolved problem where Doxygen PDF generation for OpenBLT generated errors. Default Doxygen generated format is now PDF again, instead of CHM.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@538 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Doc/doxygen/DoxyfileBootCommander.bat
+++ Doc/doxygen/DoxyfileBootCommander.bat
@@ -3,13 +3,9 @@
 :: - DoxyGen (doxygen.exe)
 :: - Graphviz (dot.exe)
 :: - MikTex (pdflatex.exe)
-:: - HTML Help Workshop compiler (hhc.exe)
 if exist ..\RM_BootCommander.pdf del ..\RM_BootCommander.pdf
 if exist ..\RM_BootCommander.chm del ..\RM_BootCommander.chm
 doxygen.exe DoxyfileBootCommander
-:: call .\output\BootCommander\latex\make.bat
-:: call copy .\output\BootCommander\latex\refman.pdf ..\RM_BootCommander.pdf
-:: Remove the comment on the next line to automatically open the generated PDF file
-:: call start "C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" ..\RM_BootCommander.pdf
-:: Remove the comment on the next line to automatically open the generated CHM file
-:: start .\..\RM_BootCommander.chm
+call .\output\BootCommander\latex\make.bat
+call copy .\output\BootCommander\latex\refman.pdf ..\RM_BootCommander.pdf
+
--- Doc/doxygen/DoxyfileSeedNKey.bat
+++ Doc/doxygen/DoxyfileSeedNKey.bat
@@ -3,13 +3,9 @@
 :: - DoxyGen (doxygen.exe)
 :: - Graphviz (dot.exe)
 :: - MikTex (pdflatex.exe)
-:: - HTML Help Workshop compiler (hhc.exe)
 if exist ..\RM_SeedNKey.pdf del ..\RM_SeedNKey.pdf
 if exist ..\RM_SeedNKey.chm del ..\RM_SeedNKey.chm
 doxygen.exe DoxyfileSeedNKey
-:: call .\output\SeedNKey\latex\make.bat
-:: call copy .\output\SeedNKey\latex\refman.pdf ..\RM_SeedNKey.pdf
-:: Remove the comment on the next line to automatically open the generated PDF file
-:: call start "C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" ..\RM_SeedNKey.pdf
-:: Remove the comment on the next line to automatically open the generated CHM file
-:: start .\..\RM_SeedNKey.chm
+call .\output\SeedNKey\latex\make.bat
+call copy .\output\SeedNKey\latex\refman.pdf ..\RM_SeedNKey.pdf
+
