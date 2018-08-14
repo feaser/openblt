a689918de869812cae7a137fe9f36386bd8ae0bf
Frank Voorburg <voorburg@feaser.com>
2018-08-14 12:53:30 +0000

    Refs #434. Converted STM32-H103/GCC demo programs from SPL to HAL.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/branches/sandbox@601 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- stm32_spl_to_hal/Target/Demo/ARMCM3_STM32F1_Olimex_STM32H103_GCC/Boot/flash_layout.c
+++ stm32_spl_to_hal/Target/Demo/ARMCM3_STM32F1_Olimex_STM32H103_GCC/Boot/flash_layout.c
@@ -37,22 +37,22 @@ static const tFlashSector flashLayout[] =
    * interfaces enabled. when for example only UART is needed, than the space required
    * for the bootloader can be made a lot smaller here.
    */
-  /* { 0x08000000, 0x02000,  0},           flash sector  0 - reserved for bootloader   */
-  /* { 0x08002000, 0x02000,  1},           flash sector  1 - reserved for bootloader   */
-  { 0x08004000, 0x02000,  2},           /* flash sector  2 - 8kb                       */
-  { 0x08006000, 0x02000,  3},           /* flash sector  3 - 8kb                       */
-  { 0x08008000, 0x02000,  4},           /* flash sector  4 - 8kb                       */
-  { 0x0800A000, 0x02000,  5},           /* flash sector  5 - 8kb                       */
-  { 0x0800C000, 0x02000,  6},           /* flash sector  6 - 8kb                       */
-  { 0x0800E000, 0x02000,  7},           /* flash sector  7 - 8kb                       */
-  { 0x08010000, 0x02000,  8},           /* flash sector  8 - 8kb                       */
-  { 0x08012000, 0x02000,  9},           /* flash sector  9 - 8kb                       */
-  { 0x08014000, 0x02000, 10},           /* flash sector 10 - 8kb                       */
-  { 0x08016000, 0x02000, 11},           /* flash sector 11 - 8kb                       */
-  { 0x08018000, 0x02000, 12},           /* flash sector 12 - 8kb                       */
-  { 0x0801A000, 0x02000, 13},           /* flash sector 13 - 8kb                       */
-  { 0x0801C000, 0x02000, 14},           /* flash sector 14 - 8kb                       */
-  { 0x0801E000, 0x02000, 15},           /* flash sector 15 - 8kb                       */
+  /* { 0x08000000, 0x02000 },           flash sector  0 - reserved for bootloader   */
+  /* { 0x08002000, 0x02000 },           flash sector  1 - reserved for bootloader   */
+  { 0x08004000, 0x02000 },           /* flash sector  2 - 8kb                       */
+  { 0x08006000, 0x02000 },           /* flash sector  3 - 8kb                       */
+  { 0x08008000, 0x02000 },           /* flash sector  4 - 8kb                       */
+  { 0x0800A000, 0x02000 },           /* flash sector  5 - 8kb                       */
+  { 0x0800C000, 0x02000 },           /* flash sector  6 - 8kb                       */
+  { 0x0800E000, 0x02000 },           /* flash sector  7 - 8kb                       */
+  { 0x08010000, 0x02000 },           /* flash sector  8 - 8kb                       */
+  { 0x08012000, 0x02000 },           /* flash sector  9 - 8kb                       */
+  { 0x08014000, 0x02000 },           /* flash sector 10 - 8kb                       */
+  { 0x08016000, 0x02000 },           /* flash sector 11 - 8kb                       */
+  { 0x08018000, 0x02000 },           /* flash sector 12 - 8kb                       */
+  { 0x0801A000, 0x02000 },           /* flash sector 13 - 8kb                       */
+  { 0x0801C000, 0x02000 },           /* flash sector 14 - 8kb                       */
+  { 0x0801E000, 0x02000 },           /* flash sector 15 - 8kb                       */
 };
 
 
