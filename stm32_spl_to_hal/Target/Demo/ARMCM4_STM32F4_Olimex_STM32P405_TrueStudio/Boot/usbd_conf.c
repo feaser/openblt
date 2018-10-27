3a7651a30fe26a26064e839635f28a89a342c626
Frank Voorburg <voorburg@feaser.com>
2018-10-27 11:09:25 +0000

    Refs #434. Removed USB interrupt configuration.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/branches/sandbox@636 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- stm32_spl_to_hal/Target/Demo/ARMCM4_STM32F4_Olimex_STM32P405_TrueStudio/Boot/usbd_conf.c
+++ stm32_spl_to_hal/Target/Demo/ARMCM4_STM32F4_Olimex_STM32P405_TrueStudio/Boot/usbd_conf.c
@@ -119,9 +119,6 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
     /* Peripheral clock enable */
     __HAL_RCC_USB_OTG_FS_CLK_ENABLE();
 
-    /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(OTG_FS_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(OTG_FS_IRQn);
   /* USER CODE BEGIN USB_OTG_FS_MspInit 1 */
 
   /* USER CODE END USB_OTG_FS_MspInit 1 */
@@ -143,9 +140,6 @@ void HAL_PCD_MspDeInit(PCD_HandleTypeDef* pcdHandle)
     PA12     ------> USB_OTG_FS_DP 
     */
     HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
-
-    /* Peripheral interrupt Deinit*/
-    HAL_NVIC_DisableIRQ(OTG_FS_IRQn);
 
   /* USER CODE BEGIN USB_OTG_FS_MspDeInit 1 */
 
