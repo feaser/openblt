6b934fe394950379c321a8c2f252d0113f18da04
Frank Voorburg <voorburg@feaser.com>
2018-12-19 16:54:37 +0000

    Refs #434. Merged latest changes from the trunk.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/branches/sandbox@659 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- stm32_spl_to_hal/Target/Demo/ARMCM4_STM32F4_Olimex_STM32E407_TrueStudio/Boot/blt_conf.h
+++ stm32_spl_to_hal/Target/Demo/ARMCM4_STM32F4_Olimex_STM32E407_TrueStudio/Boot/blt_conf.h
@@ -171,7 +171,7 @@
  *         bootloader application can explicitly initialize the communication interface
  *         by calling ComDeferredInit().
  */
-#define BOOT_COM_NET_DEFERRED_INIT_ENABLE (0)
+#define BOOT_COM_NET_DEFERRED_INIT_ENABLE (1)
 
 
 /****************************************************************************************
