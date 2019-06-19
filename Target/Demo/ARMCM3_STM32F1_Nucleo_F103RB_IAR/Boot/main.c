23a2e095c9eda582d99e34cd5848b562fc3ee596
Frank Voorburg <voorburg@feaser.com>
2019-06-19 16:52:33 +0000

    Refs #822. Changed UART Rx pin mode to floating for the Nucleo-F103RB demo bootloader.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@698 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Target/Demo/ARMCM3_STM32F1_Nucleo_F103RB_IAR/Boot/main.c
+++ Target/Demo/ARMCM3_STM32F1_Nucleo_F103RB_IAR/Boot/main.c
@@ -175,11 +175,11 @@ void HAL_MspInit(void)
   /* UART TX and RX GPIO pin configuration. */
   GPIO_InitStruct.Pin = LL_GPIO_PIN_2;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
   GPIO_InitStruct.Pin = LL_GPIO_PIN_3;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_FLOATING;
   LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 #endif
 } /*** end of HAL_MspInit ***/
