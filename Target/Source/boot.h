fa4f1b63f28963093180fa6e077af2c290de0ccf
Frank Voorburg <voorburg@feaser.com>
2019-07-03 06:21:57 +0000

    Refs #845. Updated version numbers and Windows binaries in preparation for new release.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@704 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Target/Source/boot.h
+++ Target/Source/boot.h
@@ -34,7 +34,7 @@
 /** \brief Main version number of the bootloader core. */
 #define BOOT_VERSION_CORE_MAIN     (1u)
 /** \brief Minor version number of the bootloader core. */
-#define BOOT_VERSION_CORE_MINOR    (7u)
+#define BOOT_VERSION_CORE_MINOR    (8u)
 /** \brief Patch number of the bootloader core. */
 #define BOOT_VERSION_CORE_PATCH    (0u)
 
