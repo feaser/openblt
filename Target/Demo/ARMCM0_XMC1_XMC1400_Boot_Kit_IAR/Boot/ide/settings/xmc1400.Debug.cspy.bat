8ec8ee7816e2a6bd0a8ed7372b9df91ab3c4b47d
Frank Voorburg <voorburg@feaser.com>
2019-08-08 16:06:22 +0000

    Refs #521. Updated all IAR demo programs to v7.80 to v8.40 of EWARM.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@711 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Target/Demo/ARMCM0_XMC1_XMC1400_Boot_Kit_IAR/Boot/ide/settings/xmc1400.Debug.cspy.bat
+++ Target/Demo/ARMCM0_XMC1_XMC1400_Boot_Kit_IAR/Boot/ide/settings/xmc1400.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Work\software\OpenBLT\Target\Demo\ARMCM0_XMC1_XMC1400_Boot_Kit_IAR\Boot\ide\settings\xmc1400.Debug.general.xcl" --backend -f "C:\Work\software\OpenBLT\Target\Demo\ARMCM0_XMC1_XMC1400_Boot_Kit_IAR\Boot\ide\settings\xmc1400.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Work\software\OpenBLT\Target\Demo\ARMCM0_XMC1_XMC1400_Boot_Kit_IAR\Boot\ide\settings\xmc1400.Debug.general.xcl" --backend -f "C:\Work\software\OpenBLT\Target\Demo\ARMCM0_XMC1_XMC1400_Boot_Kit_IAR\Boot\ide\settings\xmc1400.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Work\software\OpenBLT\Target\Demo\ARMCM0_XMC1_XMC1400_Boot_Kit_IAR\Boot\ide\settings\xmc1400.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Work\software\OpenBLT\Target\Demo\ARMCM0_XMC1_XMC1400_Boot_Kit_IAR\Boot\ide\settings\xmc1400.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Work\software\OpenBLT\Target\Demo\ARMCM0_XMC1_XMC1400_Boot_Kit_IAR\Boot\ide\settings\xmc1400.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Work\software\OpenBLT\Target\Demo\ARMCM0_XMC1_XMC1400_Boot_Kit_IAR\Boot\ide\settings\xmc1400.Debug.driver.xcl" 
 
 @echo off 
 :end
