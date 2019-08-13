e0011ae70d19a8791b7dc0ca2184a374ce8f7ba2
Frank Voorburg <voorburg@feaser.com>
2019-08-13 13:43:27 +0000

    Refs #818. Merged branch for STM32G0 port development back into the trunk.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@718 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

---
+++ Target/Demo/ARMCM0_STM32G0_Nucleo_G071RB_GCC/Prog/main.c
@@ -0,0 +1,228 @@
+/************************************************************************************//**
+* \file         Demo/ARMCM0_STM32G0_Nucleo_G071RB_GCC/Prog/main.c
+* \brief        Demo program application source file.
+* \ingroup      Prog_ARMCM0_STM32G0_Nucleo_G071RB_GCC
+* \internal
+*----------------------------------------------------------------------------------------
+*                          C O P Y R I G H T
+*----------------------------------------------------------------------------------------
+*   Copyright (c) 2019  by Feaser    http://www.feaser.com    All rights reserved
+*
+*----------------------------------------------------------------------------------------
+*                            L I C E N S E
+*----------------------------------------------------------------------------------------
+* This file is part of OpenBLT. OpenBLT is free software: you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as published by the Free
+* Software Foundation, either version 3 of the License, or (at your option) any later
+* version.
+*
+* OpenBLT is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE. See the GNU General Public License for more details.
+*
+* You have received a copy of the GNU General Public License along with OpenBLT. It 
+* should be located in ".\Doc\license.html". If not, contact Feaser to obtain a copy.
+* 
+* \endinternal
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include "header.h"                                    /* generic header               */
+
+
+/****************************************************************************************
+* Function prototypes
+****************************************************************************************/
+static void Init(void);
+static void SystemClock_Config(void);
+
+
+/************************************************************************************//**
+** \brief     This is the entry point for the bootloader application and is called 
+**            by the reset interrupt vector after the C-startup routines executed.
+** \return    Program return code.
+**
+****************************************************************************************/
+int main(void)
+{
+  /* initialize the microcontroller */
+  Init();
+  /* initialize the bootloader interface */
+  BootComInit();
+
+  /* start the infinite program loop */
+  while (1)
+  {
+    /* toggle LED with a fixed frequency */
+    LedToggle();
+    /* check for bootloader activation request */
+    BootComCheckActivationRequest();
+  }
+
+  /* program should never get here */
+  return 0;
+} /*** end of main ***/
+
+
+/************************************************************************************//**
+** \brief     Initializes the microcontroller.
+** \return    none.
+**
+****************************************************************************************/
+static void Init(void)
+{
+  /* reset of all peripherals, initializes the Flash interface and the Systick. */
+  HAL_Init();
+  /* configure the system clock. */
+  SystemClock_Config();
+  /* initialize the timer driver */
+  TimerInit();
+  /* initialize the led driver */
+  LedInit();
+} /*** end of Init ***/
+
+
+/************************************************************************************//**
+** \brief     System Clock Configuration. This code was created by CubeMX and configures
+**            the system clock.
+** \return    none.
+**
+****************************************************************************************/
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct   = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct   = {0};
+
+  /* Configure the main internal regulator output voltage */
+
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /*
+   * PLL configuration is based on HSI/4 (4 MHz) input clock and a VCO
+   * frequency equal to four times the required output frequency (which
+   * must be an exact multiple of 1 MHz in the range 16..64 MHz).
+   */
+#define PLL_CLK_SPEED_KHZ   (HSI_VALUE / (4u * 1000u))
+
+#define PLL_N_VALUE         (4u * (BOOT_CPU_SYSTEM_SPEED_KHZ / \
+                                   PLL_CLK_SPEED_KHZ))
+  
+  /* Initialise the CPU, AHB and APB bus clocks */
+
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv              = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM            = RCC_PLLM_DIV4;
+  RCC_OscInitStruct.PLL.PLLN            = PLL_N_VALUE;
+  RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ            = RCC_PLLQ_DIV4;
+  RCC_OscInitStruct.PLL.PLLR            = RCC_PLLR_DIV4;
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Clock configuration incorrect or hardware failure. Hang the system to prevent
+     * damage.
+     */
+    while(1);
+  }
+
+  /* Initialise the CPU, AHB and APB bus clocks, set flash latency */
+  RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK   |
+                                     RCC_CLOCKTYPE_SYSCLK |
+                                     RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    /* Flash latency configuration incorrect or hardware failure. Hang the system to
+     * prevent damage.
+     */
+    while(1);
+  }
+} /*** end of SystemClock_Config ***/
+
+
+/************************************************************************************//**
+** \brief     Initializes the Global MSP. This function is called from HAL_Init()
+**            function to perform system level initialization (GPIOs, clock, DMA,
+**            interrupt).
+** \return    none.
+**
+****************************************************************************************/
+void HAL_MspInit(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* SYSCFG clock enable. */
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+
+  /* GPIO ports clock enable. */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+#if (BOOT_COM_UART_ENABLE > 0)
+  /* Peripheral clock enable. */
+  __HAL_RCC_USART2_CLK_ENABLE();
+#endif /* BOOT_COM_UART_ENABLE > 0 */
+
+  /* SVC_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SVC_IRQn, 0, 0);
+  /* PendSV_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(PendSV_IRQn, 0, 0);
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+  /* Configure the LED GPIO pin. */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+#if (BOOT_COM_UART_ENABLE > 0)
+  /* UART TX and RX GPIO pin configuration. */
+  GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF1_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+#endif /* BOOT_COM_UART_ENABLE > 0 */
+} /*** end of HAL_MspInit ***/
+
+
+/************************************************************************************//**
+** \brief     Deinitializes the Global MSP. This function is called from HAL_DeInit()
+**            function to perform system level Deinitialization (GPIOs, clock, DMA,
+**            interrupt).
+** \return    none.
+**
+****************************************************************************************/
+void HAL_MspDeInit(void)
+{
+#if (BOOT_COM_UART_ENABLE > 0)
+  /* Reset UART GPIO pin configuration. */
+  HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2 | GPIO_PIN_3);
+#endif /* BOOT_COM_UART_ENABLE > 0 */
+  /* Deconfigure GPIO pin for the LED. */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+  HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5);
+
+#if (BOOT_COM_UART_ENABLE > 0)
+  /* Peripheral clock disable. */
+  __HAL_RCC_USART2_CLK_DISABLE();
+#endif /* BOOT_COM_UART_ENABLE > 0 */
+  /* GPIO ports clock disable. */
+  __HAL_RCC_GPIOB_CLK_DISABLE();
+  __HAL_RCC_GPIOA_CLK_DISABLE();
+  /* SYSCFG clock disable. */
+  __HAL_RCC_SYSCFG_CLK_DISABLE();
+} /*** end of HAL_MspDeInit ***/
+
+
+/*********************************** end of main.c *************************************/
