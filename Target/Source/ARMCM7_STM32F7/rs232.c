54e93de20d5711e46fd2c0f8d3dc0a7cdd45da39
Frank Voorburg <voorburg@feaser.com>
2020-09-21 11:23:46 +0000

    Refs #981. Added STM32CubeIDE demo programs for the Nucleo-F767ZI board.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@796 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Target/Source/ARMCM7_STM32F7/rs232.c
+++ Target/Source/ARMCM7_STM32F7/rs232.c
@@ -100,6 +100,8 @@ void Rs232Init(void)
             (BOOT_COM_RS232_CHANNEL_INDEX == 6) ||
             (BOOT_COM_RS232_CHANNEL_INDEX == 7));
 
+  /* disable the UART peripheral */
+  LL_USART_Disable(USART_CHANNEL);
   /* configure UART peripheral */
   USART_InitStruct.BaudRate = BOOT_COM_RS232_BAUDRATE;
   USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
