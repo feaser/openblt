6ab4c83ba738e4a3446b509164ae220b21047975
Frank Voorburg <voorburg@feaser.com>
2023-01-09 08:46:55 +0000

    Refs #1652. Final changes before the next release.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@1010 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Host/Source/BootCommander/main.c
+++ Host/Source/BootCommander/main.c
@@ -472,7 +472,7 @@ int main(int argc, char const * const argv[])
 static void DisplayProgramInfo(void)
 {
   printf("--------------------------------------------------------------------------\n");
-  printf("BootCommander version 1.04.03. Performs firmware updates on a micro-\n");
+  printf("BootCommander version 1.04.04. Performs firmware updates on a micro-\n");
   printf("controller based system that runs the OpenBLT bootloader.\n\n");
   printf("Copyright (c) 2017 by Feaser  http://www.feaser.com\n");
   printf("-------------------------------------------------------------------------\n");
--- Target/Source/boot.h
+++ Target/Source/boot.h
@@ -34,7 +34,7 @@
 /** \brief Main version number of the bootloader core. */
 #define BOOT_VERSION_CORE_MAIN     (1u)
 /** \brief Minor version number of the bootloader core. */
-#define BOOT_VERSION_CORE_MINOR    (14u)
+#define BOOT_VERSION_CORE_MINOR    (15u)
 /** \brief Patch number of the bootloader core. */
 #define BOOT_VERSION_CORE_PATCH    (0u)
 
