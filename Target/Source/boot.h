57b517eabeb37d945606e18736e106bde34010bc
Frank Voorburg <voorburg@feaser.com>
2023-07-07 10:29:56 +0000

    Refs #1802. Final changes before the next release.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@1080 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Target/Source/boot.h
+++ Target/Source/boot.h
@@ -34,7 +34,7 @@
 /** \brief Main version number of the bootloader core. */
 #define BOOT_VERSION_CORE_MAIN     (1u)
 /** \brief Minor version number of the bootloader core. */
-#define BOOT_VERSION_CORE_MINOR    (15u)
+#define BOOT_VERSION_CORE_MINOR    (16u)
 /** \brief Patch number of the bootloader core. */
 #define BOOT_VERSION_CORE_PATCH    (0u)
 
