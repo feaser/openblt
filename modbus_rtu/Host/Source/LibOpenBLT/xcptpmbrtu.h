ae24d0c5e5266c1e11587505022343e43077c961
Frank Voorburg <voorburg@feaser.com>
2023-11-22 10:20:16 +0000

    Refs #789. Added Modbus RTU support to the host tools.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/branches/sandbox@1116 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- modbus_rtu/Host/Source/LibOpenBLT/xcptpmbrtu.h
+++ modbus_rtu/Host/Source/LibOpenBLT/xcptpmbrtu.h
@@ -11,19 +11,17 @@
 *----------------------------------------------------------------------------------------
 *                            L I C E N S E
 *----------------------------------------------------------------------------------------
-* This file is part of OpenBLT. It is released under a commercial license. Refer to
-* the license.pdf file for the exact licensing terms. It should be located in
-* '.\Doc\license.pdf'. Contact Feaser if you did not receive the license.pdf file.
-* 
-* In a nutshell, you are allowed to modify and use this software in your closed source
-* proprietary application, provided that the following conditions are met:
-* 1. The software is not released or distributed in source (human-readable) form.
-* 2. These licensing terms and the copyright notice above are not changed.
-* 
-* This software has been carefully tested, but is not guaranteed for any particular
-* purpose. Feaser does not offer any warranties and does not guarantee the accuracy,
-* adequacy, or completeness of the software and is not responsible for any errors or
-* omissions or the results obtained from use of the software.
+* This file is part of OpenBLT. OpenBLT is free software: you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as published by the Free
+* Software Foundation, either version 3 of the License, or (at your option) any later
+* version.
+*
+* OpenBLT is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE. See the GNU General Public License for more details.
+*
+* You have received a copy of the GNU General Public License along with OpenBLT. It
+* should be located in ".\Doc\license.html". If not, contact Feaser to obtain a copy.
 *
 * \endinternal
 ****************************************************************************************/
