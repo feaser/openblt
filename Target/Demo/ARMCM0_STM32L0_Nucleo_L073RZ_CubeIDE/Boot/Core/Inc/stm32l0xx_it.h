2761b24f0946497e02722a6b0bcea0fa32c9f124
Frank Voorburg <voorburg@feaser.com>
2026-02-13 10:56:56 +0000

    Refs #1983, #2411, #2412, #2420, #2421. Upgraded the STM32L0 and STM32L1 demo programs to the latest HAL/LL drivers, CubeIDE 1.19.0, GCC 13.3, IAR 9.70.1, and the Keil V6 compiler.
    git-svn-id: https://svn.code.sf.net/p/openblt/code/trunk@1252 5dc33758-31d5-4daf-9ae8-b24bf3d40d73

--- Target/Demo/ARMCM0_STM32L0_Nucleo_L073RZ_CubeIDE/Boot/Core/Inc/stm32l0xx_it.h
+++ Target/Demo/ARMCM0_STM32L0_Nucleo_L073RZ_CubeIDE/Boot/Core/Inc/stm32l0xx_it.h
@@ -23,7 +23,7 @@
 #define __STM32L0xx_IT_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Private includes ----------------------------------------------------------*/
--- Target/Demo/ARMCM3_STM32L1_Nucleo_L152RE_CubeIDE/Boot/Core/Src/main.c
+++ Target/Demo/ARMCM3_STM32L1_Nucleo_L152RE_CubeIDE/Boot/Core/Src/main.c
@@ -228,8 +228,8 @@ static void MX_USART2_UART_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -252,8 +252,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -274,8 +274,7 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
